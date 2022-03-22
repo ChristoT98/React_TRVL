f6407333faf3220614eae8bf89bb717e6df3013d
ChristoT98 <christolakshan@gmail.com>
2022-03-22 11:35:07 +0530

    Navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import "./App.css";
+// import Home from './components/pages/Home';
+// import Services from './components/pages/Services';
+// import Products from './components/pages/Products';
+// import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        <Routes >
+        <Routes>
           <Route path="/" exact />
-        </Routes >
+          {/* <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} /> */}
+        </Routes>
       </Router>
     </>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,78 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Button } from "../button/Button";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 
 export default function Navbar() {
+  const [openMenu, setOpenMenu] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const openMenuHandler = () => setOpenMenu(!openMenu);
+  const closeMobileMenuHandler = () => setOpenMenu(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
   return (
     <>
       <nav className="navbar">
         <div className="navbarContainer">
-          <Link to="/" className="navbarLogo">
+          <Link to="/" className="navbarLogo" onClick={closeMobileMenuHandler}>
             TRVL <i className="fab fa-typo3" />
           </Link>
+          <div className="menuIcon" onClick={openMenuHandler}>
+            <i className={openMenu ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={openMenu ? "navMenu active" : "navMenu"}>
+            <li className="navItem">
+              <Link
+                to="/"
+                className="navLinks"
+                onClick={closeMobileMenuHandler}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="navItem">
+              <Link
+                to="/services"
+                className="navLinks"
+                onClick={closeMobileMenuHandler}
+              >
+                Services
+              </Link>
+            </li>
+            <li className="navItem">
+              <Link
+                to="/products"
+                className="navLinks"
+                onClick={closeMobileMenuHandler}
+              >
+                Products
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/signUp"
+                className="navLinksMobile"
+                onClick={closeMobileMenuHandler}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle={"btn--outline"}>SIGN UP</Button>}
         </div>
       </nav>
     </>
